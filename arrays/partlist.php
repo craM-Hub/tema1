77289e8718544e57aad908a13f27bfeac3fd1067
craM-Hub <anarka84@gmail.com>
2021-10-06 11:20:05 +0200

    partList with function

--- arrays/partlist.php
+++ arrays/partlist.php
@@ -5,14 +5,18 @@ $numPalabras = count($cadena);
 
 $resultado = [];
 
-
+function partList($cadena, $pos)
+{
+    $pareja = [];
+    array_push($pareja, implode(' ', array_slice($cadena, 0, $pos)));
+    array_push($pareja, implode(' ', array_slice($cadena, $pos, 5)));
+    return ($pareja);
+}
 
 
 for ($i = 1; $i < $numPalabras; $i++) {
-    $pareja = [];
-    array_push($pareja, implode(' ', array_slice($cadena, 0, $i)));
-    array_push($pareja, implode(' ', array_slice($cadena, $i, 5)));
-    array_push($resultado, $pareja);
+
+    array_push($resultado, partList($cadena, $i));
 }
 
 print_r($resultado);
